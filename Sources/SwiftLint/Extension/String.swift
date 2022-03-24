96747762b88f1d3f683513548efaf1d75379df64
codelint <unotseeme@foxmail.com>
2022-03-24 16:55:21 +0800

    enhance

---
+++ Sources/SwiftLint/Extension/String.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  
+//
+//  Created by gzhang on 2022/3/24.
+//
+
+import Foundation
+
+extension String {
+    func starts(with: [String]) -> Bool {
+        for w in with {
+            if self.starts(with: w) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    func match(regex: String) -> Bool {
+        let pre = NSPredicate(format: "SELF MATCHES %@", regex)
+        
+        return pre.evaluate(with: self);
+    }
+}
