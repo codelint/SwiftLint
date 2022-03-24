9a1448a7693ff8db1d491a0ca6d9600de9b59e51
codelint <unotseeme@foxmail.com>
2022-03-24 18:14:53 +0800

    add trst for Array.asyncEach

---
+++ Tests/SwiftLintTests/Extension/CollectionTest.swift
@@ -0,0 +1,54 @@
+//
+//  File.swift
+//  
+//
+//  Created by gzhang on 2022/3/24.
+//
+
+import XCTest
+@testable import SwiftLint
+
+class CollectionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        
+        super.tearDown()
+    }
+    
+    func testArrayAsyncEach() {
+        // helper.findBy(request: NSFetchRequest<User>)
+        let arr = [5, 2, 0, 1, 3, 1, 4]
+        var firstE = 0
+        var lastE = 0
+        
+        arr.asyncEach(arr, next: { item, index, next in
+            XCTAssertEqual(item, arr[index])
+            next()
+        }, first: { first, next in
+            firstE = first
+            next()
+        }) { last in
+            lastE = last
+        }
+        XCTAssertEqual(firstE, 5)
+        XCTAssertEqual(lastE, 4)
+        
+        lastE = 0
+        firstE = 0
+        arr.asyncEach(arr, from: 1, next: { item, index, next in
+            XCTAssertTrue(false)
+            next()
+        }, first: { first, next in
+            firstE = first
+        }, last: { last in
+            lastE = last
+        })
+        XCTAssertEqual(firstE, 2)
+        XCTAssertEqual(lastE, 0)
+    }
+    
+}
