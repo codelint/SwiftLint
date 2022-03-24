1b6ad94abb31a24514b2171a6fa328046e14310c
codelint <unotseeme@foxmail.com>
2022-03-24 17:01:27 +0800

    enhance

--- Sources/SwiftLint/Extension/Date.swift
+++ Sources/SwiftLint/Extension/Date.swift
@@ -8,19 +8,32 @@
 import Foundation
 
 public extension Date {
+    
     var datetimeString: String {
-        let formatter = DateFormatter()
-        formatter.dateStyle = DateFormatter.Style.medium
-        formatter.timeStyle = DateFormatter.Style.short
-        formatter.dateFormat = "YYYY-MM-dd HH:mm:ss"
-        return formatter.string(from: self)
+        return self.string(format: "YYYY-MM-dd HH:mm:ss")
     }
     
     var dateString: String {
+        return self.string(format: "YYYY-MM-dd")
+    }
+    
+    var int64: Int64 {
+        Int64(self.timeIntervalSince1970)
+    }
+    
+    var int32: Int32 {
+        Int32(self.timeIntervalSince1970)
+    }
+    
+    var int: Int {
+        Int(self.timeIntervalSince1970)
+    }
+    
+    func string(format: String = "YYYY-MM-dd HH:mm:ss") -> String {
         let formatter = DateFormatter()
         formatter.dateStyle = DateFormatter.Style.medium
         formatter.timeStyle = DateFormatter.Style.short
-        formatter.dateFormat = "YYYY-MM-dd"
+        formatter.dateFormat = format
         return formatter.string(from: self)
     }
 }
