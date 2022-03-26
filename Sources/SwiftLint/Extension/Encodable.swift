fd43d5f80d564a29907b4640abfe37a219dcbba1
codelint <unotseeme@foxmail.com>
2022-03-26 11:14:45 +0800

    enhance

--- Sources/SwiftLint/Extension/Encodable.swift
+++ Sources/SwiftLint/Extension/Encodable.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-extension Encodable{
+public extension Encodable{
     var stringified: String? {
         let encoder = JSONEncoder()
         encoder.outputFormatting = .prettyPrinted
