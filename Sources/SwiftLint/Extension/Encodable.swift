630cf33c071ee2dd04bdcf25c341dffba053720b
codelint <unotseeme@foxmail.com>
2022-03-26 11:11:02 +0800

    enhance

---
+++ Sources/SwiftLint/Extension/Encodable.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by gzhang on 2022/3/26.
+//
+
+import Foundation
+
+extension Encodable{
+    var stringified: String? {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+        guard let data = try? encoder.encode(self) else { return nil }
+        return String(data: data, encoding: .utf8)
+    }
+}
