3acb12ecd3d8d3fc9e8dddfefa87c14be2fc6673
codelint <unotseeme@foxmail.com>
2022-04-15 10:10:06 +0800

    debug

--- Sources/SwiftLint/Util/CoreDataHelper.swift
+++ Sources/SwiftLint/Util/CoreDataHelper.swift
@@ -246,7 +246,7 @@ open class CoreDataHelper {
             }.filter({ str in
                 return str.count > 0
             }).joined(separator: " && ")
-            
+            print("predicate_str = \(predicate_str)")
             if predicate_str.count > 0 {
                 request.predicate = NSPredicate(format: predicate_str)
             }
